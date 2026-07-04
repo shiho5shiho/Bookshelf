89d891a23f9caefaa1e948c5fc07850d61329e41
shiho5shiho <shiho5shiho@gmail.com>
2026-07-04 11:54:30 +0900

    feat:認証機能の実装(Fortify) #3

--- database/seeders/FavoriteSeeder.php
+++ database/seeders/FavoriteSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\Book;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 
 class FavoriteSeeder extends Seeder
 {
@@ -18,17 +17,17 @@ class FavoriteSeeder extends Seeder
         $books = Book::all();
 
         $favorites = [
-            'yamada@example.com'    => [1, 2, 3, 4, 5],
-            'suzuki@example.com'    => [2, 4, 6, 8],
-            'tanaka@example.com'    => [1, 3, 5, 7, 9],
-            'sato@example.com'      => [3, 6, 9, 11],
+            'yamada@example.com' => [1, 2, 3, 4, 5],
+            'suzuki@example.com' => [2, 4, 6, 8],
+            'tanaka@example.com' => [1, 3, 5, 7, 9],
+            'sato@example.com' => [3, 6, 9, 11],
             'takahashi@example.com' => [2, 5, 8, 10, 11],
         ];
 
         foreach ($favorites as $email => $bookNumbers) {
             $user = $users->firstWhere('email', $email);
             // books は通常のIndex(0から始まる)で取得されるため、1から始まる番号を0から始まるIndexに変換するため$n-1を使用
-            $bookIds = collect($bookNumbers)->map(fn($n) => $books[$n - 1]->id);
+            $bookIds = collect($bookNumbers)->map(fn ($n) => $books[$n - 1]->id);
             $user->favoriteBooks()->syncWithoutDetaching($bookIds->toArray());
         }
     }
--- database/seeders/ReviewLikeSeeder.php
+++ database/seeders/ReviewLikeSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\Review;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 
 class ReviewLikeSeeder extends Seeder
 {
