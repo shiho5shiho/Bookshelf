6725aaea0202195339f84d8c5f0ca884f0a6d3c0
shiho5shiho <shiho5shiho@gmail.com>
2026-07-12 08:55:01 +0900

    feat: バリデーション・認証・エラー文言を日本語化 #19

---
+++ lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードを再設定しました。',
+    'sent' => 'パスワード再設定用のメールを送信しました。',
+    'throttled' => 'しばらく待ってから再度お試しください。',
+    'token' => 'このパスワード再設定用トークンは無効です。',
+    'user' => "該当するメールアドレスのユーザーが見つかりませんでした。",
+
+];
