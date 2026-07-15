de48e326d37826776178e082037d49243c6afead
shiho5shiho <shiho5shiho@gmail.com>
2026-07-15 22:23:40 +0900

    feat: いいね機能を実装 #11

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ReviewLikeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,19 +27,20 @@ Route::middleware('auth')->group(function () {
     Route::resource('genres', GenreController::class);
     Route::resource('books', BookController::class)->except(['index', 'show']);
 
-    // TODO: レビュー関連のルート
+    // レビュー関連のルート
     Route::post('/books/{book}/reviews', [ReviewController::class, 'store'])->name('reviews.store');
     Route::get('/reviews/{review}/edit', [ReviewController::class, 'edit'])->name('reviews.edit');
     Route::put('/reviews/{review}', [ReviewController::class, 'update'])->name('reviews.update');
     Route::delete('/reviews/{review}', [ReviewController::class, 'destroy'])->name('reviews.destroy');
 
-    // TODO: お気に入り関連のルート
+    // お気に入り関連のルート
     Route::get('/favorites', [FavoriteController::class, 'index'])->name('favorites.index');
     Route::post('/books/{book}/favorites', [FavoriteController::class, 'toggle'])->name('favorites.toggle');
 
-    // TODO: いいね機能の仮ルート(#11で本実装に置き換え)
-    Route::post('/reviews/{review}/like', fn ($review) => 'いいね機能は未実装です(Issue #11で実装予定)')->name('reviews.like');
+    // いいね機能のルート
+    Route::post('/reviews/{review}/like', [ReviewLikeController::class, 'toggle'])->name('reviews.like');
 });
 
 Route::resource('books', BookController::class)->only(['index', 'show']);
-Route::get('/ranking', fn () => 'ランキングは未実装です(Issue #12で実装予定)')->name('ranking.index');
+// TODO:ランキング機能の仮ルート（Issue #12で実装予定）
+Route::get('/ranking', fn() => 'ランキングは未実装です(Issue #12で実装予定)')->name('ranking.index');
