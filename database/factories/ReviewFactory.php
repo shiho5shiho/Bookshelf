ada2514bfe68fef02a5d7fe0c1e850b47699ec77
shiho5shiho <shiho5shiho@gmail.com>
2026-07-20 21:39:56 +0900

    test:モデルの単体テストを実装 #15

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Book;
 use App\Models\Review;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,6 +20,8 @@ class ReviewFactory extends Factory
     public function definition(): array
     {
         return [
+            'book_id' => Book::factory(),
+            'user_id' => User::factory(),
             'rating' => fake()->numberBetween(1, 5),
             'comment' => fake()->paragraph(),
         ];
