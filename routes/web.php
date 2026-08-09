6b20f7779811aa96fae56853ce0a6dee6ddecf70
shiho5shiho <shiho5shiho@gmail.com>
2026-08-09 14:00:15 +0900

    WIP: #62 通知機能実装一式（動作確認・テストは後日実施）

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\RankingController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ReviewLikeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NotificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +29,10 @@ Route::middleware('auth')->group(function () {
     // ジャンルのCRUDルート
     Route::resource('genres', GenreController::class);
     Route::resource('books', BookController::class)->except(['index', 'show']);
+
+    // 通知
+    Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
+    Route::post('/notifications/{id}/read', [NotificationController::class, 'read'])->name('notifications.read');
 
     // レビュー関連のルート
     Route::post('/books/{book}/reviews', [ReviewController::class, 'store'])->name('reviews.store');
