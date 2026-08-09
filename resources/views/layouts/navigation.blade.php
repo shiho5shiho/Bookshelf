7d5a33895e6ba0b844c62c94a49e133398aa17fc
shiho5shiho <shiho5shiho@gmail.com>
2026-08-09 11:25:58 +0900

    fix: 通知ベルアイコンのリンクにも一時的ガードを追加
    notifications.indexルート未定義によるRouteNotFoundExceptionを解消。
    Issue #62完了時にRoute::hasの判定が自動的にtrueとなる。

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -49,7 +49,9 @@
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 @auth
+                @auth
                 <!-- 通知ベルアイコン -->
+                @if (Route::has('notifications.index'))
                 <a href="{{ route('notifications.index') }}" class="relative inline-flex items-center px-3 py-2 text-gray-500 hover:text-gray-700">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
@@ -58,6 +60,7 @@
                     <span class="absolute top-0 right-0 inline-flex items-center justify-center px-1.5 py-0.5 text-xs font-bold leading-none text-white bg-red-600 rounded-full">{{ $unreadNotificationCount }}</span>
                     @endif
                 </a>
+                @endif
 
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
